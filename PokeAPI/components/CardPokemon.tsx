7bc8c47f9472a6ae607367493c99019990a731f9
Gabi <heavenxg89@gmail.com>
2026-08-17 14:31:33 -0300

    feat:adiciona arquivos

--- PokeAPI/components/CardPokemon.tsx
+++ PokeAPI/components/CardPokemon.tsx
@@ -1,35 +1,46 @@
 import React from "react";
 import { View, Text, Image, StyleSheet } from "react-native";
 
-const CardPokemon = ({ item }: { item: { name: string; url: string } }) => {
-  // Extrai o ID do Pokémon através da URL da API
+type Props = {
+  item: {
+    name: string;
+    url: string;
+  };
+};
+
+const CardPokemon = ({ item }: Props) => {
   const pokemonId = item.url.split("/").filter(Boolean).pop();
-  
-  // Cria o link dinâmico da imagem usando Template Literals (crases)
+
   const imageUrl = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokemonId}.png`;
 
   return (
     <View style={styles.card}>
       <Image source={{ uri: imageUrl }} style={styles.imagem} />
+
       <Text style={styles.nome}>{item.name}</Text>
     </View>
   );
 };
 
+export default CardPokemon;
+
 const styles = StyleSheet.create({
   card: {
-    flex: 1,
+    width: "46%",
+    height: 190,
+    marginHorizontal: "2%",
+    marginBottom: 10,
+    backgroundColor: "#c066fc",
+    borderRadius: 12,
     alignItems: "center",
     justifyContent: "center",
-    margin: 8,
-    padding: 12,
-    backgroundColor: "#f5f5f5",
-    borderRadius: 8,
   },
   imagem: {
-    width: 96,
-    height: 96,
+    width: 130,
+    height: 150,
+    resizeMode: "contain",
   },
+
   nome: {
     fontSize: 16,
     fontWeight: "bold",
@@ -37,5 +48,3 @@ const styles = StyleSheet.create({
     marginTop: 8,
   },
 });
-
-export default CardPokemon;
--- PokeAPI/components/TelaPokemon.tsx
+++ PokeAPI/components/TelaPokemon.tsx
@@ -1,46 +1,52 @@
-import { StyleSheet, View, TextInput, FlatList } from "react-native";
+import { StyleSheet, TextInput, FlatList } from "react-native";
 import React, { useEffect, useState } from "react";
 import CardPokemon from "./CardPokemon";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 const TelaPokemon = () => {
   const [nome, setNome] = useState("");
   const [pokemon, setPokemon] = useState<any[]>([]);
 
   const filtrar = pokemon.filter((item) =>
-    item.name.toLowerCase().includes(nome.toLowerCase())
+    item.name.toLowerCase().includes(nome.toLowerCase()),
   );
 
   useEffect(() => {
     const buscarPokemon = async () => {
       try {
-        console.log("Buscando dados da API");
         const response = await fetch("https://pokeapi.co/api/v2/pokemon");
+
         const dados = await response.json();
-        const quantidade = dados.results.slice(0, 10);
+
+        const quantidade = dados.results.slice(0, 20);
+
         setPokemon(quantidade);
       } catch (erro) {
         console.log("Não foi possível buscar dados", erro);
       }
     };
+
     buscarPokemon();
   }, []);
 
   return (
-    <View style={styles.container}>
+    <SafeAreaView style={styles.container}>
       <TextInput
         style={styles.input}
         value={nome}
-        placeholder="Busque seu Pokémon"
+        placeholder="🔍  Busque seu Pokémon"
         onChangeText={setNome}
       />
 
       <FlatList
         data={filtrar}
-        keyExtractor={(item) => item.name} // Ajustado para item.name
+        keyExtractor={(item) => item.name}
         numColumns={2}
         renderItem={({ item }) => <CardPokemon item={item} />}
+        contentContainerStyle={styles.lista}
+        showsVerticalScrollIndicator={false}
       />
-    </View>
+    </SafeAreaView>
   );
 };
 
@@ -49,14 +55,20 @@ export default TelaPokemon;
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    padding: 16,
+    backgroundColor: "#e4c2ff",
+    alignItems:"center"
   },
+
   input: {
-    height: 40,
-    borderColor: "#ccc",
-    borderWidth: 1,
-    borderRadius: 8,
-    paddingHorizontal: 10,
-    marginBottom: 16,
+    height: 50,
+    width: 300,
+    backgroundColor: "#fff",
+    borderRadius: 25,
+    paddingHorizontal: 15,
+    marginBottom: 15,
+  },
+
+  lista: {
+    paddingBottom: 20,
   },
 });
